ada438396f6fc6170d64be6bd61d97d439cd433f
bram <bramdenouden96@hotmail.com>
2020-02-24 18:24:19 +0100

    indexation updated

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ import os
 
 mydir = os.path.dirname(__file__)
 
+print('\n----------------------------------')
 print('I live in: ' + mydir)
 
 
 cap = pyshark.FileCapture(mydir + '/data/mycapture.pcapng')
-dir (cap[0])
+# wlan layer: wlan
+# sniff time: sniff_time
+currentCap = cap[0]
+
+print(cap[0].sniff_time)
+wlan = cap[0].wlan
+
+# receiver address : ra
+# destination address: da
+# transmitter address: ta
+# source address: sa
+# vendor lookup api https://macvendors.com/
+
+print('destination address = ' + wlan.da)
+print('source address = ' + wlan.sa)
