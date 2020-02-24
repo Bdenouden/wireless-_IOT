9c24b6a2f1f1c593049db99431bd7a6c896369c1
bram <bramdenouden96@hotmail.com>
2020-02-24 13:24:55 +0100

    packet read test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello Y"all')
+import matplotlib.pyplot
+import pyshark
+import sys
+import os
+
+mydir = os.path.dirname(__file__)
+
+print('I live in: ' + mydir)
+
+
+cap = pyshark.FileCapture(mydir + '/data/mycapture.pcapng')
+dir (cap[0])
