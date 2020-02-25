b0063c59869c6f9c9b4a2b50caecb459cb1585cc
bram <bramdenouden96@hotmail.com>
2020-02-25 14:08:35 +0100

    keep_packets turned off to save memory

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-import os
-import sys
-
+from datetime import datetime
 import matplotlib.pyplot as plt
 import pyshark
 
-dataDict = {
-
-}
-
-mydir = os.path.dirname(__file__)
+dataDict = {}
+now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+counter = 0
 
 print('\n----------------------------------')
-# print('I live in: ' + mydir)
 
+cap = pyshark.FileCapture('data/mycapture.pcapng',keep_packets=False)
 
-cap = pyshark.FileCapture(mydir + '/data/mycapture.pcapng')
 # wlan layer: wlan
 # sniff time: sniff_time
 
-for x in range(400):
+# for x in range(400):
 # for x in cap:
     # print(cap[x].sniff_time)
     # print('packet number ' + str(x))
-    try:
-        wlan = cap[x].wlan
+
+def add_data_to_dict(packet):
+        # wlan = cap[x].wlan
+        wlan = packet.wlan
+
         key = wlan.ra
         if key not in dataDict:
             dataDict[key] = 1
         else:
             dataDict[key] = dataDict[key] + 1
-    except: 
-        y = 1
-        # print('line ' + str(x) + 'could not be printed')
+        counter += 1
+
+        if counter % 100 == 0:
+            print('#',end='')
+
+#     if x % 100 == 0:
+#         print("#",end='')
+# print('\ndone!')
 # receiver address : ra
 # destination address: da
 # transmitter address: ta
 # source address: sa
 # vendor lookup api https://macvendors.com/
 
-    # print('destination address = ' + wlan.da)
-    # print('source address = ' + wlan.sa)
-    # print('\n')
+cap.apply_on_packets(add_data_to_dict)
+
 
 print(dataDict)
 plt.bar(dataDict.keys(),dataDict.values())
-plt.xticks(rotation=45)
-plt.show()
+plt.xticks(rotation=45,ha="right")
+plt.tight_layout()
+plt.savefig('figures/' + now,format='eps')
+# plt.show()
 print('-------------------------\n\n')
 
