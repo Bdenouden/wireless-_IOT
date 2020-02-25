343738c88822eec227827bd2761002a608dcd0d8
bram <bramdenouden96@hotmail.com>
2020-02-25 15:35:12 +0100

    mac lookup added

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
 import pyshark
+import os
+from mac_vendor_lookup import MacLookup
+
+# check if data and figures folder exist, if not create them
+if not os.path.exists('data'):
+    os.makedirs('data')
+    print('data folder created')
+if not os.path.exists('figures'):
+    os.makedirs('figures')
+    print('figures folder created')
+
 
 dataDict = {}
+# dataDict = [
+#   {MAC : {ra_amount: ra_amount, sa_amount: sa_amount, sniff_times: [sniff_times]}}
+# ]
 now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 counter = 0
+
+mac = MacLookup()
+mac.load_vendors()
 
 print('\n----------------------------------')
 
@@ -13,42 +30,61 @@ cap = pyshark.FileCapture('data/mycapture.pcapng',keep_packets=False)
 # wlan layer: wlan
 # sniff time: sniff_time
 
-# for x in range(400):
-# for x in cap:
-    # print(cap[x].sniff_time)
-    # print('packet number ' + str(x))
-
 def add_data_to_dict(packet):
-        # wlan = cap[x].wlan
-        wlan = packet.wlan
+    global counter
+    wlan = packet.wlan
 
-        key = wlan.ra
-        if key not in dataDict:
-            dataDict[key] = 1
+    key = wlan.ra
+    if key not in dataDict:
+        dataDict[key] = 1
+    else:
+        dataDict[key] = dataDict[key] + 1
+    counter += 1
+
+    if counter % 100 == 0:
+        print('#',end='')
+    if counter % 5000 == 0:
+        print('!!')
+
+def get_vendors(myDict):
+    vendorDict = {}
+    keys = myDict.keys()
+    print('getting vendors: ')
+    for key in keys:
+        amount = myDict[key]
+        
+        try:
+            vendor = mac.lookup(key)
+        except:
+            vendor = key
+
+        if vendor not in vendorDict:
+            vendorDict[vendor] = amount
         else:
-            dataDict[key] = dataDict[key] + 1
-        counter += 1
+            vendorDict[vendor] += amount
+        print('#',end='')
+    print('')
+    return vendorDict
 
-        if counter % 100 == 0:
-            print('#',end='')
-
-#     if x % 100 == 0:
-#         print("#",end='')
-# print('\ndone!')
 # receiver address : ra
 # destination address: da
 # transmitter address: ta
 # source address: sa
 # vendor lookup api https://macvendors.com/
 
+# cap.apply_on_packets(add_data_to_dict,packet_count=500)
 cap.apply_on_packets(add_data_to_dict)
+print('') # newline to end the progress updates
 
+vendors = get_vendors(dataDict)
+
+print(sum(dataDict.values()) == sum(vendors.values()))
 
 print(dataDict)
-plt.bar(dataDict.keys(),dataDict.values())
+print(vendors)
+plt.bar(vendors.keys(),vendors.values())
 plt.xticks(rotation=45,ha="right")
 plt.tight_layout()
 plt.savefig('figures/' + now,format='eps')
-# plt.show()
 print('-------------------------\n\n')
 
